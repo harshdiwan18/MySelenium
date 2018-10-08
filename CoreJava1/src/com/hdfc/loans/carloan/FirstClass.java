ad8c660df6be034f58b0e9c9326eafcbe17c9a36
harshdiwan18 <harshdiwan18@gmail.com>
2018-10-08 09:59:34 -0700

    This is some changes in FirstClass

---
+++ CoreJava1/src/com/hdfc/loans/carloan/FirstClass.java
@@ -0,0 +1,33 @@
+package com.hdfc.loans.carloan;
+
+public class FirstClass {
+
+	int a=10, b= 20, c; // 3 Properties
+	
+	public void  Add() 
+	{
+	
+		c=a+b;
+		
+		System.out.println("Addition of a and b is :" + c);
+		
+	}
+	
+	public void  Sub() 
+	{
+	    c=a-b;
+		
+		System.out.println("Subtraction of a and b is :" + c);
+		
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		System.out.println("This is commit practice");
+		
+		FirstClass obj= new FirstClass();
+		obj.Add();
+		obj.Sub();
+	}
+
+}
